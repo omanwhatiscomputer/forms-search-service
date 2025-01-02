d1cd4bf9835075e89599a148cdb4c3e1e01eae1a
omanwhatiscomputer <khubayeebkabeer@gmail.com>
2025-01-03 00:39:28 +0600

    cleanup

--- obj/Debug/net8.0/SearchService.AssemblyInfo.cs
+++ obj/Debug/net8.0/SearchService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SearchService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d6c5f6bac38d431ef32377d9cac12165aa52fe1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SearchService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SearchService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
